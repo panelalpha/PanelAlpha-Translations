feefa5235a58a694ea5c518901a96d3af404a9fd
Damian Wilusz <damians.wilusz@gmail.com>
2025-11-10 21:02:38 +0100

    feat(en): add email templates for server alerts and hosting account synchronization

---
+++ panelalpha/translations/en/email-templates/User/Service/SyncHostingAccountFinished/Email/message.blade.php
@@ -0,0 +1,2 @@
+<p>Dear <strong>{{ $client_first_name }}</strong>,</p>
+<p>The synchronization of your hosting account <strong>{{ $hosting_account_username }}</strong> with PanelAlpha has been successful.</p>
