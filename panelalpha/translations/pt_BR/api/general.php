e1bb3cbd0abe8a5b4e37f2f1a3247610f8f7623c
Damian Wilusz <damian.wi@modulesgarden.com>
2026-02-05 13:18:17 +0100

    Update Brazilian Portuguese translations

--- panelalpha/translations/pt_BR/api/general.php
+++ panelalpha/translations/pt_BR/api/general.php
@@ -5,6 +5,8 @@ return [
     'self_signed' => 'Assinado pelo próprio',
     'hosting_account' => 'conta de hospedagem',
     'service' => 'serviço',
+    'arquivos',
+    'banco de dados',
     'import_steps' => [
         'creating-hosting-account' => 'Criação de conta de hospedagem',
         'creating-domain' => 'Criação de domínio',
@@ -23,7 +25,24 @@ return [
         'downloading-database' => 'Download do banco de dados',
         'removing-ftp-account' => 'Remoção da conta FTP',
         'creating-temporary-directory' => 'Criação de diretório temporário',
-    ],
+        'installPluginOnNewInstance' => "Instalando o plugin 'WordPress Migrator' na nova instância",
+        'installPluginOnImportedInstance' => "Instalando o plugin 'WordPress Migrator' na instância importada",
+        'installNewInstance' => 'Instalando nova instância',
+        'prepareNewInstanceToMigration' => 'Preparando nova instância para migração',
+        'prepareMigrationUsingPlugin' => 'Preparando migração',
+        'runMigrationUsingPlugin' => 'Executando migração',
+        'prepareInstance' => 'Preparando instância para migração',
+        'downloadDatabase' => 'Baixando banco de dados',
+        'downloadFiles' => 'Baixando arquivos',
+        'uploadFiles' => 'Enviando arquivos',
+        'prepareFiles' => 'Preparando arquivos',
+        'uploadDatabase' => 'Enviando banco de dados',
+        'importDatabase' => 'Importando banco de dados',
+        'configureWordPress' => 'Configurando WordPress',
+        'cleanUp' => 'Limpando arquivos',
+        'createFtpAccount' => 'Criando conta FTP/SFTP',
+        'cleanUpPlugins' => 'Limpando plugins',
+],
     'health-checks' => [
         'server' => [
             'ping' => 'Conexão de teste do servidor',
@@ -49,4 +68,36 @@ return [
             'local_data' => 'Verificação de dados locais corretos no WordPress',
         ],
     ],
+
+    'server_alerts' => [
+        "high_disk_usage" => "High Disk Usage",
+        "high_cpu_usage" => "High CPU Usage",
+        "high_ram_usage" => "High RAM Usage",
+    ],
+    'report_providers' => [
+        'referer' => [
+            'website' => 'Websites',
+            'direct' => 'Direct Entry',
+            'search_engine' => 'Search Engines',
+            'unknown' => 'Unknown',
+            'internal' => 'Internal',
+        ]
+    ],
+    'continent' => [
+        'as' => 'Asia',
+        'eu' => 'Europe',
+        'na' => 'North America',
+        'sa' => 'South America',
+        'af' => 'Africa',
+        'oc' => 'Oceania',
+        'an' => 'Antarctica'
+    ],
+    'field' => [
+        'onboarding_domain' => [
+            'label' => 'Onboarding Domain (Optional)',
+            'description' => 'Optional, if set, it will override onboarding domain configured in plan settings.'
+        ]
+    ],
+    'files' => 'Arquivos',
+    'database' => 'Banco de dados',
 ];
